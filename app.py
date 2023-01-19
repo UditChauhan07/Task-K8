fcee8d4e8bdf78e7de1ee1dea46336e5da76f62e
Manisha148 <70441355+Manisha148@users.noreply.github.com>
2023-01-19 12:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def home():
     return render_template('index.html')
 
     
-if __name__ == '__main__':
+if _name_ == '__main__':
     app.run()
 
 
 PORT = int(os.environ.get("PORT", 9090))
-if __name__ == '__main__':
+if _name_ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=PORT)
 
-if __name__ == "__main__":
+if _name_ == "__main__":
   app.run(host='0.0.0.0', port=9090)
